768264ee124a87038bcbee99b1deff088334bbca
pepsi160ml <111164516+pepsi160ml@users.noreply.github.com>
2023-01-06 16:10:37 +0900

    1
    1

--- Test22/src/main/java/com/example/Test22/Controller.java
+++ Test22/src/main/java/com/example/Test22/Controller.java
@@ -1,9 +1,11 @@
 package com.example.Test22;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class Controller {
 
     @GetMapping("/")
